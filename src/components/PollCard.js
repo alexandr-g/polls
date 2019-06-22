fc1a4bb0d450eef2633bb64787a8b481cff8d22a
Alexander Grischuk <alexgrischuk@gmail.com>
2019-06-22 20:14:26 +0200

    Add vote button and navigation to poll details

--- src/components/PollCard.js
+++ src/components/PollCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { withRouter } from 'react-router'
 import styled from 'styled-components'
 
 const CardWrapper = styled.div`
@@ -15,28 +16,57 @@ const CardHeading = styled.h1`
   font-size: 24px;
   font-weight: bold;
   text-align: center;
+
+  &:hover {
+    cursor: pointer;
+  }
 `
 
 const CardOption = styled.small`
   margin-left: 10px;
   padding-top: 10px;
+  padding-bottom: 10px;
   display: block;
   width: 100%;
   font-size: 12px;
   text-align: left;
   text-transform: uppercase;
 `
+export const CardButton = styled.button`
+  display: block;
+  width: 100%;
+  padding: 12px 0;
+  font-family: inherit;
+  font-size: 14px;
+  font-weight: 700;
+  color: #fff;
+  background-color: #e5195f;
+  border: 0;
+  border-radius: 35px;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.08);
+  cursor: pointer;
+  transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
 
-const PollTile = ({ poll }) => {
+  &:hover {
+    box-shadow: 0 15px 15px rgba(0, 0, 0, 0.16);
+    transform: translate(0, -5px);
+  }
+`
+
+const PollCard = ({ poll, history }) => {
   return (
     <CardWrapper>
-      <CardHeading>{poll.question}</CardHeading>
+      <CardHeading onClick={() => history.push(`${poll.url}`)}>
+        {poll.question}
+      </CardHeading>
 
       <CardOption>Choices: {poll.choices.length}</CardOption>
 
       <CardOption>Published at: {poll.published_at}</CardOption>
+
+      <CardButton onClick={() => history.push(`${poll.url}`)}>Vote</CardButton>
     </CardWrapper>
   )
 }
 
-export default PollTile
+export default withRouter(PollCard)
