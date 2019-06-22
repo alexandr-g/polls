7832aaacf6c1d5eabace40e584327c6ade3edd27
Alexander Grischuk <alexgrischuk@gmail.com>
2019-06-22 11:08:08 +0200

    Improve error handling in API

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,8 @@
 export const getQuestions = () =>
-  fetch('https://polls.apiblueprint.org/questions?page=1').then(response =>
-    response.json()
-  )
+  fetch('https://polls.apiblueprint.org/questions?page=1').then(response => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      throw new Error('Something went wrong...')
+    }
+  })
