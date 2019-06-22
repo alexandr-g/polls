6edcdcfef8e393bc6988f8a0b0f17ca6a08d4840
Alexander Grischuk <alexgrischuk@gmail.com>
2019-06-22 10:36:24 +0200

    Add custom webpack, babel and initial project config

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Questions from './Questions'
+
+const App = () => {
+  return <Questions />
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'))
