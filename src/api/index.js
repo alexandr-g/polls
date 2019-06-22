855beb50c922ed9d3df9bde3c1dd4a823ba15858
Alexander Grischuk <alexgrischuk@gmail.com>
2019-06-22 11:29:13 +0200

    Add error handling in async/await

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,16 @@
 async function getQuestions() {
-  let response = await fetch('https://polls.apiblueprint.org/questions?page=1')
-  let questions = await response.json()
+  let questions
+  try {
+    let response = await fetch(
+      'https://polls.apiblueprint.org/questions?page=1'
+    )
+    if (!response.ok) {
+      throw Error(response.statusText)
+    }
+    questions = await response.json()
+  } catch (error) {
+    throw new Error('Something went wrong...')
+  }
 
   return questions
 }
