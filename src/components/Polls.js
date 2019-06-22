18485687421a4b9fe30e4dcefc7be94c51fb63b6
Alexander Grischuk <alexgrischuk@gmail.com>
2019-06-22 20:23:05 +0200

    Re-organize styles

--- src/components/Polls.js
+++ src/components/Polls.js
@@ -4,23 +4,6 @@ import styled from 'styled-components'
 import PollCard from './PollCard'
 import api from '../api/index'
 
-const PollsHeader = styled.h1`
-  padding-left: 80px;
-  color: #e5195f;
-  font-size: 40px;
-`
-
-const PollsContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  margin-top: 10px;
-  padding: 1.5%;
-  box-sizing: border-box;
-  max-width: 1100px;
-  margin: 0 auto;
-`
-
 const Polls = () => {
   const [polls, setPolls] = useState([])
   const [isLoading, setLoading] = useState(false)
@@ -51,3 +34,20 @@ const Polls = () => {
 }
 
 export default Polls
+
+const PollsHeader = styled.h1`
+  padding-left: 80px;
+  color: #e5195f;
+  font-size: 40px;
+`
+
+const PollsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin-top: 10px;
+  padding: 1.5%;
+  box-sizing: border-box;
+  max-width: 1100px;
+  margin: 0 auto;
+`
