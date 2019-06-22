0717e8fb736078cc61e3de442f690d2df7890f8d
Alexander Grischuk <alexgrischuk@gmail.com>
2019-06-22 20:12:12 +0200

    Introduce routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
+import { Route } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
+
 import Polls from './components/Polls'
+import PollDetails from './components/PollDetails'
 
-const App = () => {
-  return <Polls />
-}
-
+const App = () => (
+  <BrowserRouter>
+    <Fragment>
+      <Route exact path={['/', '/questions']} component={Polls} />
+      <Route exact path='/questions/:questionId' component={PollDetails} />
+    </Fragment>
+  </BrowserRouter>
+)
 ReactDOM.render(<App />, document.querySelector('#app'))
