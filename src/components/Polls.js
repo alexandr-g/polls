0e813377c45f235efcc0499fe52be843eeac0e6b
Alexander Grischuk <alexgrischuk@gmail.com>
2019-06-23 15:44:21 +0200

    Render create new poll page and button

--- src/components/Polls.js
+++ src/components/Polls.js
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import styled from 'styled-components'
 
 import PollCard from './PollCard'
 import Loading from './Loading'
 
-import api from '../api/index'
-
 import { Header } from './layout/Container'
+
+import api from '../api/index'
+import CreatePoll from './CreatePoll'
 
 const Polls = () => {
   const [polls, setPolls] = useState([])
   const [isLoading, setLoading] = useState(false)
+  const [createPoll, setCreatePoll] = useState(false)
 
   useEffect(() => {
     setLoading(true)
@@ -27,12 +29,21 @@ const Polls = () => {
 
   return (
     <div>
-      <Header>Questions</Header>
-      <PollsContainer>
-        {polls.map(poll => (
-          <PollCard key={poll.published_at} poll={poll} />
-        ))}
-      </PollsContainer>
+      {createPoll ? (
+        <CreatePoll />
+      ) : (
+        <Fragment>
+          <Header>Questions</Header>
+          <NewQuestionButton onClick={() => setCreatePoll(true)}>
+            Create a new question
+          </NewQuestionButton>
+          <PollsContainer>
+            {polls.map(poll => (
+              <PollCard key={poll.published_at} poll={poll} />
+            ))}
+          </PollsContainer>
+        </Fragment>
+      )}
     </div>
   )
 }
@@ -48,4 +59,26 @@ const PollsContainer = styled.div`
   box-sizing: border-box;
   max-width: 1100px;
   margin: 0 auto;
+`
+
+const NewQuestionButton = styled.button`
+  display: block;
+  width: 20%;
+  padding: 12px 0;
+  margin-left: 80px;
+  font-family: inherit;
+  font-size: 16px;
+  font-weight: 700;
+  color: #fff;
+  background-color: #3cb371;
+  border: 0;
+  border-radius: 35px;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.08);
+  cursor: pointer;
+  transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
+
+  &:hover {
+    box-shadow: 0 15px 15px rgba(0, 0, 0, 0.16);
+    transform: translate(0, -5px);
+  }
 `
