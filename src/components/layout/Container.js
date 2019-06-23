15324b86ff14879826f9aaef794727822772bcdd
Alexander Grischuk <alexgrischuk@gmail.com>
2019-06-23 14:08:58 +0200

    Add generic button style

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -1,8 +1,28 @@
-import React from 'react'
 import styled from 'styled-components'
 
 export const Header = styled.h1`
   padding-left: 80px;
   color: #e5195f;
   font-size: 40px;
+`
+
+export const Button = styled.button`
+  display: block;
+  width: 20%;
+  padding: 12px 0;
+  font-family: inherit;
+  font-size: 14px;
+  font-weight: 700;
+  color: #fff;
+  background-color: #3cb371;
+  border: 0;
+  border-radius: 35px;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.08);
+  cursor: pointer;
+  transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
+
+  &:hover {
+    box-shadow: 0 15px 15px rgba(0, 0, 0, 0.16);
+    transform: translate(0, -5px);
+  }
 `
